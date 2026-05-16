8277add08f6bb86e1faf90073f61d47b08ed5d64
KalistoSphinx <shouryadeshmukh6265@gmail.com>
2026-05-17 01:17:37 +0530

    added login page and sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Outlet } from "react-router";
+
 function App(){
-  
+  return(
+    <div className="dark flex min-h-screen w-full items-center justify-center overflow-y-auto bg-neutral-950 text-white">
+        <p
+          className="absolute left-6 top-6 z-10 text-lg font-semibold tracking-[0.35em] uppercase text-white"
+          aria-label="Xenon home"
+        >
+          Xenon
+        </p>
+        <Outlet />
+    </div>
+  )
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router"
+import { LoginPage } from './pages/loginPage.tsx'
+import { SignUpPage } from './pages/signUpPage.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App />}>
+        <Route path='' element={<LoginPage />} />
+        <Route path='signup' element={<SignUpPage />} />
+      </Route>
+
+    </>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

5e30808abae6856e390f3a43fd9d8dd305d281ea
KalistoSphinx <shouryadeshmukh6265@gmail.com>
2026-05-26 01:16:25 +0530

    added page switching

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -6,26 +6,30 @@ import {
 } from "@/components/ui/sidebar"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { Note02Icon, StarIcon, Delete02Icon } from "@hugeicons/core-free-icons"
+import { useState } from "react";
 
 export function NavMain() {
+
+  const [filter, setFilter] = useState("");
+
   return (
     <SidebarGroup>
       <SidebarMenu>
         
-        <SidebarMenuItem>
-          <SidebarMenuButton isActive={true}>
+        <SidebarMenuItem key={"notes"}>
+          <SidebarMenuButton isActive={filter == "notes"} onClick={() => setFilter("notes")}>
             <HugeiconsIcon icon={Note02Icon} strokeWidth={2}/> <span>Notes</span>
           </SidebarMenuButton>
         </SidebarMenuItem>
 
-        <SidebarMenuItem>
-          <SidebarMenuButton>
+        <SidebarMenuItem key={"starred"}>
+          <SidebarMenuButton isActive={filter == "starred"} onClick={() => setFilter("starred")}>
             <HugeiconsIcon icon={StarIcon} strokeWidth={2}/> <span>Starred</span>
           </SidebarMenuButton>
         </SidebarMenuItem>
 
-        <SidebarMenuItem>
-          <SidebarMenuButton>
+        <SidebarMenuItem key={"trash"}>
+          <SidebarMenuButton isActive={filter == "trash"} onClick={() => setFilter("trash")}>
             <HugeiconsIcon icon={Delete02Icon} strokeWidth={2}/> <span>Trash</span>
           </SidebarMenuButton>
         </SidebarMenuItem>

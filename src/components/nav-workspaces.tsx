a24c48a40685efd93c9d13c653c4bae6f57f654e
KalistoSphinx <shouryadeshmukh6265@gmail.com>
2026-06-06 02:26:04 +0530

    implemented workspace adding

--- src/components/nav-workspaces.tsx
+++ src/components/nav-workspaces.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   DropdownMenu,
@@ -6,7 +6,7 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 import {
   SidebarGroup,
   SidebarGroupAction,
@@ -16,45 +16,118 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from "@/components/ui/sidebar"
-import { HugeiconsIcon } from "@hugeicons/react"
-import { MoreHorizontalCircle01Icon, FolderIcon, Share03Icon, Delete02Icon } from "@hugeicons/core-free-icons"
-import { Plus } from "lucide-react"
-import { Popover, PopoverContent, PopoverDescription, PopoverHeader, PopoverTitle, PopoverTrigger } from "./ui/popover"
-import { Input } from "./ui/input"
-import { Button } from "./ui/button"
+} from "@/components/ui/sidebar";
+import { HugeiconsIcon } from "@hugeicons/react";
+import {
+  MoreHorizontalCircle01Icon,
+  FolderIcon,
+  Share03Icon,
+  Delete02Icon,
+} from "@hugeicons/core-free-icons";
+import { Plus } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverDescription,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from "./ui/popover";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { Field, FieldError } from "./ui/field";
+import { api } from "@/lib/api";
 
 export function NavWorkspaces({
   workspaces,
 }: {
   workspaces: {
-    name: string
-    url: string
-    color: string,
-  }[]
+    name: string;
+    url: string;
+    color: string;
+  }[];
 }) {
-  const { isMobile } = useSidebar()
+  const { isMobile } = useSidebar();
+
+  const [workspaceName, setWorkspaceName] = useState("");
+  const [workspaceColor, setWorkspaceColor] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [open, setOpen] = useState(false)
+
+  const handleCreate = async () => {
+    const name = workspaceName.trim();
+
+    if(!name){
+      setNameError("Name is required");
+      return
+    }
+
+    setNameError("");
+
+    try {
+      await api.post("/workspaces", {
+        name: name,
+        color: workspaceColor
+      })
+
+      setOpen(false)
+      console.log("Hello");
+      
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <SidebarGroup className="group">
       <SidebarGroupLabel>Workspaces</SidebarGroupLabel>
       <SidebarGroupAction className="opacity-0 transition-opacity duration-200 ease-in-out group-hover:opacity-100">
-        <Popover>
-          <PopoverTrigger render={
-            <Plus strokeWidth={2} size={15} className="text-sidebar-foreground/70 outline-none" />
-          } />
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger
+            render={
+              <Plus
+                strokeWidth={2}
+                size={15}
+                className="text-sidebar-foreground/70 outline-none"
+              />
+            }
+          />
           <PopoverContent align="start" className={"border border-border pt-0"}>
             <PopoverHeader>
               <PopoverTitle>Create Workspace</PopoverTitle>
-              <PopoverDescription>Add a new workspace to organize your projects.</PopoverDescription>
+              <PopoverDescription>
+                Add a new workspace to organize your projects.
+              </PopoverDescription>
             </PopoverHeader>
             <div className="flex flex-col gap-4">
-              <div className="flex gap-2 items-center">
+              <div className="flex gap-2 items-start">
                 <div className="relative flex h-8 w-8 shrink-0 items-center justify-center rounded-xl border border-border overflow-hidden">
-                  <Input type="color" className="absolute -left-2 -top-2 h-12 w-12 cursor-pointer border-0 p-0" defaultValue="#3b82f6" />
+                  <Input
+                    type="color"
+                    className="absolute -left-2 -top-2 h-12 w-12 cursor-pointer border-0 p-0"
+                    defaultValue="#3b82f6"
+                    onChange={(e) => setWorkspaceColor(e.target.value)}
+                  />
                 </div>
-                <Input type="text" placeholder="Workspace name" className="h-8 flex-1"/>
+                <Field data-invalid={!!nameError} className="flex flex-col flex-1 gap-1.5">
+                  <Input
+                  type="text"
+                  placeholder="Workspace name"
+                  aria-invalid={!!nameError}
+                  className="h-8"
+                  onChange={(e) => {
+                    setWorkspaceName(e.target.value)
+                    if(nameError) setNameError("");
+                  }}
+                />
+                {nameError && (<FieldError>{nameError}</FieldError>)}
+                </Field>
               </div>
-              <Button size="sm" className="w-full">Create</Button>
+              <Button size="sm" className="w-full" onClick={handleCreate}>
+                Create
+              </Button>
             </div>
           </PopoverContent>
         </Popover>
@@ -63,9 +136,12 @@ export function NavWorkspaces({
         {workspaces.map((item) => (
           <SidebarMenuItem key={item.name}>
             <SidebarMenuButton render={<a href={item.url} />}>
-              <span className={`size-1.5 rounded-full `} style={{
-                backgroundColor: item.color,
-              }}/>
+              <span
+                className={`size-1.5 rounded-full `}
+                style={{
+                  backgroundColor: item.color,
+                }}
+              />
               <span>{item.name}</span>
             </SidebarMenuButton>
             <DropdownMenu>
@@ -77,7 +153,10 @@ export function NavWorkspaces({
                   />
                 }
               >
-                <HugeiconsIcon icon={MoreHorizontalCircle01Icon} strokeWidth={2} />
+                <HugeiconsIcon
+                  icon={MoreHorizontalCircle01Icon}
+                  strokeWidth={2}
+                />
                 <span className="sr-only">More</span>
               </DropdownMenuTrigger>
               <DropdownMenuContent
@@ -86,16 +165,28 @@ export function NavWorkspaces({
                 align={isMobile ? "end" : "start"}
               >
                 <DropdownMenuItem>
-                  <HugeiconsIcon icon={FolderIcon} strokeWidth={2} className="text-muted-foreground" />
+                  <HugeiconsIcon
+                    icon={FolderIcon}
+                    strokeWidth={2}
+                    className="text-muted-foreground"
+                  />
                   <span>View Project</span>
                 </DropdownMenuItem>
                 <DropdownMenuItem>
-                  <HugeiconsIcon icon={Share03Icon} strokeWidth={2} className="text-muted-foreground" />
+                  <HugeiconsIcon
+                    icon={Share03Icon}
+                    strokeWidth={2}
+                    className="text-muted-foreground"
+                  />
                   <span>Share Project</span>
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem variant="destructive">
-                  <HugeiconsIcon icon={Delete02Icon} strokeWidth={2} className="text-muted-foreground" />
+                  <HugeiconsIcon
+                    icon={Delete02Icon}
+                    strokeWidth={2}
+                    className="text-muted-foreground"
+                  />
                   <span>Delete Project</span>
                 </DropdownMenuItem>
               </DropdownMenuContent>
@@ -104,5 +195,5 @@ export function NavWorkspaces({
         ))}
       </SidebarMenu>
     </SidebarGroup>
-  )
+  );
 }
